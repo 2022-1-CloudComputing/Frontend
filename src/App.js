8ecd1e2cbdac18889df93b605b7ebc60988753ee
sw0000j <sw0000j@gmail.com>
2022-05-17 15:28:17 +0900

    [ADD]-LoginPage Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import FavoritesPage from './components/pages/FavoritesPage';
 import SharePage from './components/pages/SharePage';
+import LoginPage from './components/pages/LoginPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/file" element={<FilePage />} />
         <Route path="/share" element={<SharePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </div>
   );
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/total.css'
+import axios from 'axios';
 
 const LoginPage = () => {
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPw, setInputPw] = useState('');
+
+  const inputEmailHandler = (email) => {
+    setInputEmail(email.target.value);
+  }
+
+  const inputPwHandler = (pw) => {
+    setInputPw(pw.target.value);
+  }
+
+  const loginButtonHandler = () => {
+    console.log('Login');
+  }
+
+  //<div className="text-3xl font-bold underline h-6">
   return (
-    <div className="text-3xl font-bold underline h-6">
-      <h1>Login Page</h1>
+    <div>
+      <div className='login-wrapper'>
+        <h1 className='font-bold text-gray-800 login-ou-header'>
+            <Link to='/'>OU</Link>
+        </h1>
+        <div className='login-box-wrapper'>
+          <div className='login-container'>
+            <h2 className='login-header'>
+              Login
+            </h2>
+            <div className='login-input-container'>
+              <label htmlFor='input_email'>Email</label>
+              <input type='text' name='input_email' value={inputEmail} onChange={inputEmailHandler} />
+            </div>
+            <div className='login-input-container'>
+              <label htmlFor='input_pw'>PW</label>
+              <input type='password' name='input_pw' value={inputPw} onChange={inputPwHandler} />
+            </div>
+            <div className='login-button-container'>
+              <input type='submit' onClick={loginButtonHandler} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
